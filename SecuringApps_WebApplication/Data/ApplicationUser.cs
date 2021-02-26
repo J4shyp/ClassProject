3860d382a157543fed692be9b4ebfbc9c0326b72
Joshua <joshua.portelli.c10270@mcast.edu.mt>
2021-02-26 21:29:12 +0100

    26/02 part2

---
+++ SecuringApps_WebApplication/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecuringApps_WebApplication.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Address { get; set; }
+        public string RecoveryEmail { get; set; }
+    }
+}
